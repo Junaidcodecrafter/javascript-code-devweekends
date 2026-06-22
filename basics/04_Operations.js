c2a92dc3d6b3500eb177a2c667cfea7514cdaa12
Junaid Mehmood <170196903+Junaidcodecrafter@users.noreply.github.com>
2026-06-22 13:46:46 +0000

    Update 04_Operations.js with basic operations

---
+++ basics/04_Operations.js
@@ -0,0 +1,23 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// Basic operations
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2%3); // This gives us remainder.
+// console.log(2**5); // Double ** is used for power this means 2^5.
+// console.log(3/2);
+
+let a1 = "Assalam O Alikum "
+let a2 = "Junaid"
+let a3 = a1 + a2 // This will concatinate both strings.
+console.log(a3);
+
+console.log("1" + 2 + 2) // This will concatinate all three which will result in 122 not 14.
+console.log(1 + 2 + "2") // In this, the first 2 numbers will be added and result will be 32 instead of 122.
+
+let score = 100
+console.log(++score) // This is preincrement.
+console.log(score++) // This is postincrement.
